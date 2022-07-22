16400f1678f20d765cea45e65bf6d2451110902b
Tanmay Patil <92677342+TanmayPatil105@users.noreply.github.com>
2022-07-22 22:52:01 +0530

    Add files via upload
    added getCpu

--- src/fetch.cpp
+++ src/fetch.cpp
@@ -18,13 +18,54 @@ string gethostname(string path)
 	return hostname;
 }
 
-string get_os_name()
+string getUpTime(string path)
 {
-	ifstream fin;
-	fin.open("/etc/os-release");
-	string os;
-	getline(fin, os);
-	return os.substr(13, os.length() - 14);
+    fstream fptr;
+	fptr.open(path, ios::in);
+	string time;
+
+	getline(fptr, time);
+    time=time.substr(0, time.find(" "));
+
+    int m = stoi(time)/60;
+    int h = m/60;
+    int d=h/24;
+
+    string timeS;
+    if(h==0)
+    {
+        timeS = to_string(m%60)+" mins";
+    }
+    else if(d==0)
+    {
+        timeS = to_string(h%24) + " hours, " + to_string(m%60)+" mins";
+    }
+    else
+    {
+        timeS = to_string(d) + " d, " + to_string(h%24) + " hours, " + to_string(m%60)+" mins";
+    }
+    
+    return timeS;
+}
+
+string getCpu(string path)
+{
+    fstream fptr;
+	fptr.open(path, ios::in);
+    string cpu,line,sub;
+
+    while(fptr)
+    {
+        getline(fptr,line);
+        sub=line.substr(0,10);
+        if(sub=="model name")
+        {
+            break;
+        }
+    }
+    cpu=line.substr(line.find(":")+2);
+
+    return cpu;
 }
 
 int main()
@@ -32,6 +73,8 @@ int main()
 	string user = getuser();
 	string hostname = gethostname("/etc/hostname");
 	cout << user << "@" << hostname << endl;
-	string os = get_os_name();
-	cout << os;
+	string upTime = getUpTime("/proc/uptime");
+    cout<<upTime<<endl;
+    string cpu = getCpu("/proc/cpuinfo");
+    cout<<cpu<<endl;
 }
