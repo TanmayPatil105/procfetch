b0af1f3c41006bf7f784023dcba82f8b10e8b024
Shun Phoenix <76583147+shunphoenix55@users.noreply.github.com>
2022-10-06 22:56:00 +0530

    Add elementary OS ascii

--- src/fetch.cpp
+++ src/fetch.cpp
@@ -552,7 +552,8 @@ void print()
     ascii_arts["EndeavourOS"] = {"endeavouros.ascii"};
     ascii_arts["Pop!_OS"] = {"pop!_os.ascii"};
     ascii_arts["Gentoo"] = {"gentoo.ascii"};
-    
+    ascii_arts["Elementary"] = {"elementaryos.ascii"};
+
 
     for(auto it=ascii_arts.begin(); it!=ascii_arts.end(); it++)
     {
