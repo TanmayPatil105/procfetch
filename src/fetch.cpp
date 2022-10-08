468823fb6d76ec908057a8e5ebb62cd1e89855d9
Shun Phoenix <76583147+shunphoenix55@users.noreply.github.com>
2022-10-08 11:53:46 +0530

    Add slackware ascii

--- src/fetch.cpp
+++ src/fetch.cpp
@@ -553,6 +553,7 @@ void print()
     ascii_arts["Pop!_OS"] = {"pop!_os.ascii"};
     ascii_arts["Gentoo"] = {"gentoo.ascii"};
     ascii_arts["Elementary"] = {"elementaryos.ascii"};
+    ascii_arts["Slackware"] = {"slackware.ascii"};
 
 
     for(auto it=ascii_arts.begin(); it!=ascii_arts.end(); it++)
