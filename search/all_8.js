190c6da2099ca7164f639080f6e56b1a7d2a9882
TanmayPatil105 <92677342+TanmayPatil105@users.noreply.github.com>
2022-12-07 14:12:59 +0000

    Deploying to gh-pages from @ TanmayPatil105/procfetch@045a2e076952e3de88d5a99db619f6a5e085f218 🚀

--- search/all_8.js
+++ search/all_8.js
@@ -3,5 +3,5 @@ var searchData=
   ['path_35',['Path',['../classPath.html',1,'']]],
   ['print_36',['print',['../fetch_8cpp.html#ad6d5ed4efb6721304d00c1277bff90fd',1,'print(string color_name, string distro_name):&#160;fetch.cpp'],['../fetch_8h.html#a73f2fe526cbdeb94007aabce5d5b080f',1,'print(string color, string distro_name):&#160;fetch.cpp']]],
   ['print_5fprocess_37',['print_process',['../fetch_8cpp.html#a8760435a6d7f54b4983543d29b5bb722',1,'fetch.cpp']]],
-  ['procfetch_20_3ca_20href_3d_22https_3a_2f_2fcodecov_2eio_2fgh_2ftanmaypatil105_2fprocfetch_22_3e_3cimg_20src_3d_22https_3a_2f_2fcodecov_2eio_2fgh_2ftanmaypatil105_2fprocfetch_2fbranch_2fmain_2fgraph_2fbadge_2esvg_3ftoken_3dqr6jgv3862_22_20alt_3d_22codecov_22_2f_3e_3c_2fa_3e_38',['procfetch &lt;a href=&quot;https://codecov.io/gh/TanmayPatil105/procfetch&quot;&gt;&lt;img src=&quot;https://codecov.io/gh/TanmayPatil105/procfetch/branch/main/graph/badge.svg?token=QR6JGV3862&quot; alt=&quot;codecov&quot;/&gt;&lt;/a&gt;',['../index.html',1,'']]]
+  ['procfetch_38',['procfetch',['../index.html',1,'']]]
 ];
