6aa37dde8f1b6a4854291b2116b0345c3acc488c
Tanmay Patil <tanmaynpatil105@gmail.com>
2024-06-13 17:07:34 +0530

    Do not color the delimiter

--- src/main.cpp
+++ src/main.cpp
@@ -47,6 +47,7 @@ int main(int argc, char *argv[])
  */
 void DisplayInfo(bool show_battery)
 {
+    string delim = " : "s;
     auto title = Crayon{}.bright().green();
     auto ye = Crayon{}.yellow();
     string username = ye.text(getuser()) + "@" +
@@ -54,41 +55,41 @@ void DisplayInfo(bool show_battery)
     cout << Crayon{}.underscore().text(username) << endl;
     cout << endl;
 
-    cout << title.text("OS : ") << getOS("/etc/os-release")
+    cout << title.text("OS") << delim << getOS("/etc/os-release")
          << getHardwarePlatform() << endl;
-    cout << title.text("Host : ") << getHost("/sys/devices/virtual/dmi/id/")
+    cout << title.text("Host") << delim << getHost("/sys/devices/virtual/dmi/id/")
          << endl;
-    cout << title.text("Kernel : ") << getKernel("/proc/sys/kernel/osrelease")
+    cout << title.text("Kernel") << delim << getKernel("/proc/sys/kernel/osrelease")
          << endl;
-    cout << title.text("UpTime : ") << getUpTime("/proc/uptime") << endl;
-    cout << title.text("RAM : ") << getRAM("/proc/meminfo") << endl;
-    cout << title.text("shell : ") << getSHELL("/etc/passwd") << endl;
-    cout << title.text("DE : ") << getDE() << endl;
+    cout << title.text("UpTime") << delim << getUpTime("/proc/uptime") << endl;
+    cout << title.text("RAM") << delim << getRAM("/proc/meminfo") << endl;
+    cout << title.text("shell") << delim << getSHELL("/etc/passwd") << endl;
+    cout << title.text("DE") << delim << getDE() << endl;
 
     if (resCheck())
     {
-        cout << title.text("Resolution : ")
+        cout << title.text("Resolution") << delim
              << getRES("/sys/class/graphics/fb0/modes") << endl;
     }
 
-    cout << title.text("Theme : ") << getTheme() << endl;
-    cout << title.text("Icons : ") << getIcons() << endl;
-    cout << title.text("CPU : ") << getCPU("/proc/cpuinfo") << endl;
+    cout << title.text("Theme") << delim << getTheme() << endl;
+    cout << title.text("Icons") << delim << getIcons() << endl;
+    cout << title.text("CPU") << delim << getCPU("/proc/cpuinfo") << endl;
 
     if (CpuTempCheck())
     {
         int temp = getCPUtemp("/sys/class/thermal/thermal_zone0/temp");
-        cout << title.text("CPU Temperature : ") << float(temp / 1000.0)
+        cout << title.text("CPU Temperature") << delim << float(temp / 1000.0)
              << " °C" << endl;
     }
 
     vector<string> gpus = getGPU();
     for (auto gpu : gpus)
     {
-        cout << title.text("GPU : ") << gpu << endl;
+        cout << title.text("GPU") << delim << gpu << endl;
     }
 
-    cout << title.text("Packages : ") << getPackages() << endl;
+    cout << title.text("Packages") << delim << getPackages() << endl;
 
     if (show_battery)
     {
